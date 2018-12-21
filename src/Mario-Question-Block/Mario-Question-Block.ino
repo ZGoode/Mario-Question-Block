6ea52ab3e745c64461accf3c2c12808edb060e78
Zachary Nathan Goode <zachary@the-goodes.com>
2018-12-20 21:55:11 -0800

    update

--- src/Mario-Question-Block/Mario-Question-Block.ino
+++ src/Mario-Question-Block/Mario-Question-Block.ino
@@ -1,16 +1,24 @@
 #include "ADXL335.h"
+#include "coin.h"
+#include <PCM.h>
 
-/***********************************************************************************************
-   Connect X_OUT to A0
-   Connect Y_OUT to A1
-   Connect Z_OUT to A2
-*/
+/**********************************************************************
+    Connect X_OUT to A0
+    Connect Y_OUT to A1
+    Connect Z_OUT to A2
+
+    Connect transistor for light to pin 8
+    Connect transistor for speaker to pin 11
+**********************************************************************/
 
 ADXL335 accelerometer;
+const int light = 8;
+
+void toggleLight();
 
 void setup() {
-  Serial.begin(9600);
   accelerometer.begin();
+  pinMode(light, OUTPUT);
 }
 
 void loop() {
@@ -19,12 +27,20 @@ void loop() {
   float az;
 
   accelerometer.getAcceleration(&ax, &ay, &az);
-  Serial.println("accleration of X/Y/Z: ");
-  Serial.print(ax);
-  Serial.println(" g");
-  Serial.print(ay);
-  Serial.println(" g");
-  Serial.print(az);
-  Serial.println(" g");
-  delay(500);
+
+  if ((ax > /*acceleration threshold*/) || (ay > /*acceleration threshold*/) || (az > /*acceleration threshold*/)) {
+    toggleLight();
+    startPlayback(data, NUM_ELEMENTS);
+    delay(/*length of soundbite in ms*/);
+    stopPlayback();
+    delay(/*choose time before trigger of light again*/);
+  }
+}
+
+void toggleLight() {
+  if (digitalRead(light) == HIGH) {
+    digitalWrite(light, LOW);
+  } else if (digitalRead(8) == LOW) {
+    digitalWrite(light, HIGH);
+  }
 }
