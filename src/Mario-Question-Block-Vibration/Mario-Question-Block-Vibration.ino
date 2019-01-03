0dfc3d5647ed59bb58462f0ffb7c59b88b525767
Zachary Nathan Goode <zachary@the-goodes.com>
2019-01-02 19:51:18 -0800

    Update Mario-Question-Block-Vibration.ino

--- src/Mario-Question-Block-Vibration/Mario-Question-Block-Vibration.ino
+++ src/Mario-Question-Block-Vibration/Mario-Question-Block-Vibration.ino
@@ -1,5 +1,7 @@
 #include "coin.h"
 #include "iris_gate.h"
+#include "power_up.h"
+#include "power_up_appears.h"
 #include <PCM.h>
 
 /**********************************************************************
@@ -23,7 +25,7 @@ void setup() {
 void loop() {
   int val;
   val = digitalRead(vibration);
-  
+
   if (val == 1) {
     toggleLight();
   }
@@ -38,9 +40,21 @@ void toggleLight() {
     delay(delayValue);
   } else if (digitalRead(light) == LOW) {
     digitalWrite(light, HIGH);
-    startPlayback(coin, NUM_ELEMENTS_COIN);
-    delay(419);
-    stopPlayback();
+    int rng = random(3);
+
+    if (rng == 0) {
+      startPlayback(coin, NUM_ELEMENTS_COIN);
+      delay(419);
+      stopPlayback();
+    } else if (rng == 1) {
+      startPlayback(power_up, NUM_ELEMENTS_POWER_UP);
+      delay(846);
+      stopPlayback();
+    } else if (rng == 2) {
+      startPlayback(power_up_appears, NUM_ELEMENTS_POWER_UP_APPEARS);
+      delay(467);
+      stopPlayback();
+    }
     delay(delayValue);
   }
 }
